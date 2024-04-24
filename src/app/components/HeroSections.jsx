2fc54c272a53a20e2f0f61118010c807ed4480d9
CharlieNndwa <charlienndwa100@gmail.com>
2024-04-23 23:48:13 -0700

    first commit

--- src/app/components/HeroSections.jsx
+++ src/app/components/HeroSections.jsx
@@ -13,7 +13,7 @@ const HeroSections = () => {
         <div className="col-span-7 place-self-center text-center sm:text-lrft">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold ">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-red-600">
-              Hello, I'm {""} {/* Escape with single quotes */}
+              Hello, I'm {''} {/* Escape with single quotes */}
             </span>
 
             <br />
@@ -38,21 +38,17 @@ const HeroSections = () => {
             Johannesburg, South Africa.{" "}
           </p>
           <div>
-            <Link
-              href="/#contact"
-              className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-red-500 hover:bg-slate-200 text-white"
-            >
+            <Link href="/#contact" className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-red-500 hover:bg-slate-200 text-white mt-3">
               Hire Me
             </Link>
-            <Link
-              href="/images/C.V .pdf"
-              download="My_CV.pdf"
-              className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-red-500 hover:bg-slate-200 text-white"
-            >
+            
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-red-500 hover:bg-slate-800 text-white  mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
+                <Link href="/images/C.V .pdf" download="My_CV.pdf">
+                  Download CV
+                </Link>
               </span>
-            </Link>
+            </button>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
