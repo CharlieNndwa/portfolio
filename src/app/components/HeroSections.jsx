f6cac2d862ef862813655b1993fae6ba259df8f3
CharlieNndwa <charlienndwa100@gmail.com>
2024-04-23 23:36:27 -0700

    first commit

--- src/app/components/HeroSections.jsx
+++ src/app/components/HeroSections.jsx
@@ -38,9 +38,9 @@ const HeroSections = () => {
             Johannesburg, South Africa.{" "}
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-red-500 hover:bg-slate-200 text-white">
+            <Link href="/#contact" className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-red-500 hover:bg-slate-200 text-white">
               Hire Me
-            </button>
+            </Link>
             <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-red-500 hover:bg-slate-800 text-white  mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 <Link href="/images/C.V .pdf" download="My_CV.pdf">
